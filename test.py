68fd6a1e5bdb6ca0a7c83b7c109b8da48e4ab569
Neil Manvar <ndmanvar@yahoo.com>
2018-03-16 14:05:15 -0700

    removing global vars and using self

--- test.py
+++ test.py
@@ -8,34 +8,32 @@ import os
 
 class Test:
     def setup_method(self, test_method):
-        global driver
-        driver = webdriver.Chrome()
+
+        self.driver = webdriver.Chrome()
 
         # set session id as tag
-        driver.get("file:///Users/neilmanvar/PycharmProjects/sentry-in-selenium/app/app.html")
+        self.driver.get("file:///Users/neilmanvar/PycharmProjects/sentry-in-selenium/app/app.html")
 
         # set session_id as Raven tag
-        global session_id
-        session_id= driver.session_id
-        print("Selenium session id is : %s" % session_id)
-        set_session_id_str = "Raven.setTagsContext({'selenium-session-id': '%s'})" % session_id  # need to clean up
+        self.session_id = self.driver.session_id
+        print("Selenium session id is : %s" % self.session_id)
+        set_session_id_str = "Raven.setTagsContext({'selenium-session-id': '%s'})" % self.session_id  # need to clean up
         try:
-            driver.execute_script(set_session_id_str) # error being thrown by selenium even though tag is succesfully set
+            self.driver.execute_script(set_session_id_str) # error being thrown by selenium even though tag is succesfully set
         except:
             print("Sentry selenium session-id tag set")
 
 
     def teardown_method(self, test_method):
         time.sleep(5) # sleep for short time to make sure Sentry event goes out
-        has_errors = driver.execute_script("return Raven.lastEventId()") != None
+        has_errors = self.driver.execute_script("return Raven.lastEventId()") != None
 
-        driver.close()
+        self.driver.close()
         if (has_errors):
-            # TODO: hit Sentry issues rest api (using selenium session-id as param) to obtain issues/errors and mssage into test result report
             time.sleep(180) # sleep for X seconds due to event ingestion
             url = "https://sentry.io/api/0/projects/testorg-az/sentry-in-selenium/issues/"
             querystring = {
-                "query": "selenium-session-id:%s" % session_id,
+                "query": "selenium-session-id:%s" % self.session_id,
                 "limit": 25
                 }
             headers = {
@@ -49,6 +47,6 @@ class Test:
 
     def test_sampletest(self):
         # Test actions
-        driver.find_element_by_id("button-1").click()
-        driver.find_element_by_id("button-2").click()
+        self.driver.find_element_by_id("button-1").click()
+        self.driver.find_element_by_id("button-2").click()
         assert 3 == 4 # fail on purpose
