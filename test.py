408930964f5b29bc45385ff69a0c52d5a716e75b
Neil Manvar <neil@sentry.io>
2019-05-03 13:29:14 -0700

    use discover api

--- test.py
+++ test.py
@@ -48,31 +48,46 @@ class Test:
         self.driver.quit()
         if (has_errors):
             time.sleep(5)
-            url = "https://sentry.io/api/0/projects/testorg-az/sentry-in-selenium/events/"
-            querystring = {
-                # "query": "selenium-session-id:%s" % self.session_id,
-                "limit": 25
-                }
+            # url = "https://sentry.io/api/0/projects/testorg-az/sentry-in-selenium/events/"
+            # querystring = {
+            #     # "query": "selenium-session-id:%s" % self.session_id,
+            #     "limit": 25
+            #     }
+            # headers = {
+            #     'authorization': "Bearer %s" % os.environ.get('SENTRY_AUTH_TOKEN')
+            # }
+            # response = requests.request("GET", url, headers=headers, params=querystring)
+            # json_data = json.loads(response.text)
+
+            print("\n\n-------- JS Errors (powered by Sentry) --------")
+            total_errors = 0
+
+            url = "https://sentry.io/api/0/organizations/testorg-az/discover/query/"
+
+            payload = "{\"orderby\":\"-time\",\"fields\":[\"id\",\"issue.id\",\"message\",\"error.type\",\"stack.filename\",\"stack.abs_path\",\"stack.lineno\",\"stack.function\",\"stack.colno\"],\"aggregations\":[[\"uniq\",\"id\",\"uniq_id\"]],\"range\":\"14d\",\"limit\":5000,\"conditions\":[[\"selenium-session-id\",\"=\",\"%s\"]],\"projects\":[261820,269722,1261841,1309317,208111,1367923,1316401,265601,288413,1319181,297017,1213077,1241612,1284466,234931,1318230,1235156,302369,1407826,228369,228436,228429,228037,1366275,1316515,1445069,1356467,1364701,300067,1257489,1366201,1241611,1313442,1314943,1267200,280125,1313446,1445070,262853,280389,1399858,1232459,1434630,1233912,1232461,1241613,1248558,272622,272646,261718,1313427,1317411,232874,1190123,1192562,272623,1410711,258421,259091,1326318,1271980,1197098,229477,252389,279552,1364567,240874,1226692,797920,298739,230024,227943,258917,1324056,1277950,226673,260278,1231280,228490,225677,304007,226985,1388365,1336446,1255106,252393,1298448,1201524,1385152],\"groupby\":[\"time\"],\"rollup\":86400}" % self.session_id
             headers = {
-                'authorization': "Bearer %s" % os.environ.get('SENTRY_AUTH_TOKEN')
+                'authorization': "Bearer %s" % os.environ.get('SENTRY_AUTH_TOKEN'),
+                'content-type': "application/json"
             }
-            response = requests.request("GET", url, headers=headers, params=querystring)
+
+            response = requests.request("POST", url, data=payload, headers=headers)
+
+            print(response.text)
             json_data = json.loads(response.text)
 
-            print("\n\n-------- JS Errors (courtesy of Sentry) --------")
-            total_errors = 0
-            for event in json_data:
-                for tag in event['tags']:
-                    if tag['key'] == 'selenium-session-id' and tag['value'] == self.session_id:
-                        total_errors += 1
-                        print("Error #%s:" % total_errors)
-                        print("\t%s" % event['message'])
-                        for frame in reversed(event['entries'][0]['data']['values'][0]['stacktrace']['frames']):
-                            print("\t\tat %s (%s:%s:%s)" %
-                                  (frame['function'] or '?', frame['filename'], frame['lineNo'], frame['colNo']))
-                        print(('\n\tLink to Sentry Issue/Error: '
-                               'https://sentry.io/testorg-az/sentry-in-selenium/issues/%s/events/%s/\n' %
-                               (event['groupID'], event['eventID'])))  # todo, link to event?
+            for data in json_data['data']:
+                message = data['message']
+                print("\t%s" % message)
+
+                print(('\n\tLink to Sentry Issue/Error: '
+                       'https://sentry.io/testorg-az/sentry-in-selenium/issues/%s/events/%s/\n' %
+                       (data['issue.id'], data['id'])))  # todo, link to event?
+
+                stackIndexes = range(len(data['stack.function'])).reverse()
+                for i in stackIndexes:
+                    print(i)
+                    print("\t\tat %s (%s:%s:%s)" % (data['stack.function'][i] or '?', data['stack.filename'][i], data['stack.lineno'][i], data['stack.colno'][i]))
+
 
     def test_sampletest(self):
         # Test actions
